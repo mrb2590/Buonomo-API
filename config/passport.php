833709a283b7b13f6a16fb5b3434da0828a85bf3
Mike Buonomo <mrb2590@gmail.com>
2019-09-15 18:34:57 -0400

    Store passport client IDs and secrets

--- config/passport.php
+++ config/passport.php
@@ -17,4 +17,12 @@ return [
 
     'public_key' => env('PASSPORT_PUBLIC_KEY'),
 
+    'personal_access_client_id' => env('PASSPORT_PERSONAL_ACCESS_CLIENT_ID'),
+
+    'personal_access_client_secret' => env('PASSPORT_PERSONAL_ACCESS_CLIENT_SECRET'),
+
+    'password_grant_client_id' => env('PASSPORT_PASSWORD_GRANT_CLIENT_ID'),
+
+    'password_grant_client_secret' => env('PASSPORT_PASSWORD_GRANT_CLIENT_SECRET'),
+
 ];
