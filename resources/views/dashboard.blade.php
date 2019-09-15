33e860fe2778cf213c5b2689d6d9c3108ff4d1a3
Mike Buonomo <mrb2590@gmail.com>
2019-09-15 15:23:03 -0400

    Installed Laravel Passport

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-8 mb-4">
             <div class="card">
                 <div class="card-header">Dashboard</div>
 
@@ -17,6 +17,15 @@
                     You are logged in!
                 </div>
             </div>
+        </div>
+        <div class="col-md-8 mb-4">
+            <passport-clients></passport-clients>
+        </div>
+        <div class="col-md-8 mb-4">
+            <passport-personal-access-tokens></passport-personal-access-tokens>
+        </div>
+        <div class="col-md-8">
+            <passport-authorized-clients></passport-authorized-clients>
         </div>
     </div>
 </div>
