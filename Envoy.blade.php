e482e5c57fb086a015b17900df0caa5eba9e4d01
Mike Buonomo <mrb2590@gmail.com>
2019-09-15 21:52:48 -0400

    Add Envoy deploy script

---
+++ Envoy.blade.php
@@ -0,0 +1,31 @@
+@include('vendor/autoload.php')
+
+@servers(['prod' => '127.0.0.1'])
+
+@task('deploy', ['on' => 'prod', 'confirm' => true])
+    cd /var/www/sites/buonomo-api
+
+    @if ($branch)
+        git pull origin {{ $branch }}
+    @endif
+
+    php artisan down
+
+    php artisan view:clear
+
+    php artisan config:clear
+
+    php artisan route:clear
+
+    git pull origin master --force
+
+    php artisan migrate --force
+
+    composer install --optimize-autoloader --no-dev
+
+    php artisan config:cache
+
+    php artisan route:cache
+
+    php artisan up
+@endtask
